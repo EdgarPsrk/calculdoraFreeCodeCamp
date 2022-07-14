6e6cbcdc890a8166f8b5337d02f842e671d881d9
Edgar Palomino <erreapesrk@gmail.com>
2022-07-14 15:31:49 -0500

    agregando el logo de freecodecamp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import freeCodeCampLog from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLog}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
     </div>
   );
 }
